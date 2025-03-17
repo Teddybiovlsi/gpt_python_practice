f7fc86505cdbc1b0d6eb8fd530f6dc0d73205dfd
Teddybiovlsi <M11113005@yuntech.edu.tw>
2025-03-17 12:58:56 +0800

    Add additional expert-level list comprehension examples in Python

--- List Comprehension/list_comprehension_expert.py
+++ List Comprehension/list_comprehension_expert.py
@@ -36,3 +36,43 @@ print(result)
 #     new_matrix = []
 # print(final_matrix)
 
+# expert 3
+# 數字映射成英文字母
+# 給定一個數字列表 nums，將其轉換成對應的 小寫英文字母：
+nums = [1, 5, 26, 30, 10]
+
+print([chr(num + 96) if 1 <= num <= 26 else "?" for num in nums ])
+
+# expert 4
+# 交集過濾
+# 給定兩個數字列表 list1 和 list2，請使用 列表解析式 找出它們的交集，並返回新的列表
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 6, 7]
+# 第一種解法(使用「交集」方式)
+common = list(set(list1) & set(list2))
+# 第二種解法
+common = [x for x in list1 if x in list2]
+
+print(common)
+
+# expert 5
+# 給定一個字典 scores，請使用 字典解析式 篩選出分數 >= 60 的學生
+scores = {"Alice": 85, "Bob": 55, "Charlie": 78, "David": 42}
+
+print({name:score for name, score in scores.items() if score >= 60 })
+
+# expert 6
+# 轉置矩陣
+matrix = [[1, 2, 3], 
+          [4, 5, 6]]
+
+print([[row[i] for row in matrix] for i in range(len(matrix[0]))])
+# 推導式
+# for i in range(len(matrix[0])):
+#     for row in matrix:
+#         print(row[i])
+
+
+
+
+
